f2b2b66d25ee402de2a1c7272c41f0f6898e0bcc
Wellington Leite <wellingtonleitee.s@gmail.com>
2022-03-13 20:14:46 -0300

    add mongodb and initiate an api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import dotenv from 'dotenv'
+import 'dotenv/config'
+import express from 'express';
+import routes from './routes';
 import TwitchClient from './twitch.config';
 
-dotenv.config();
-
 const twitchClient = new TwitchClient();
+const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 twitchClient.initialize();
+
+app.listen('3333')
